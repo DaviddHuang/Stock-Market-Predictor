9c1acff2747247c2a4835592a5a0ad4f2b7683ed
Davidhuang8 <davidhuang698@gmail.com>
2024-10-01 14:04:15 -0700

    improved precision score

--- stockmarket.py
+++ stockmarket.py
@@ -12,7 +12,7 @@ sp500["Tomorrow"] = sp500["Close"].shift(-1)
 sp500["Target"] = (sp500["Tomorrow"] > sp500["Close"]).astype(int) #setting target as if price icnreased or decreased
 sp500 = sp500.loc["1990-01-01":].copy() #removes data before 1990
 
-model = RandomForestClassifier(n_estimators = 100, min_samples_split= 100, random_state = 1)
+model = RandomForestClassifier(n_estimators = 200, min_samples_split= 50, random_state = 1)
 train = sp500.iloc[:-100]
 test = sp500.iloc[-100:]
 predictors = ["Close", "Volume", "Open", "High", "Low"]
@@ -23,7 +23,9 @@ combined = pd.concat([test["Target"], preds], axis = 1)
 
 def predict(train, test, predictors, model):
     model.fit(train[predictors], train["Target"])
-    preds = model.predict(test[predictors])
+    preds = model.predict_proba(test[predictors])[:, 1]
+    preds[preds >= .6] = 1
+    preds[preds < .6] = 0
     preds = pd.Series(preds, index=test.index, name = "Predictions")
     combined = pd.concat([test["Target"], preds], axis = 1)
     return combined
@@ -37,5 +39,17 @@ def backtest(data, model, predictors, start=2500, step=250):
         all_predictions.append(predictions)
     return pd.concat(all_predictions)
 
-predictions = backtest(sp500, model, predictors)
+horizons = [2,5,60,250,1000]
+new_predictors = []
+
+for horizon in horizons:
+    rolling_averages = sp500.rolling(horizon).mean()
+    ratio_column = f"Close_Ratio_{horizon}"
+    sp500[ratio_column] = sp500["Close"] / rolling_averages["Close"]
+    trend_column = f"Trend_{horizon}"
+    sp500[trend_column] = sp500.shift(1).rolling(horizon).sum()["Target"]
+    new_predictors += [ratio_column, trend_column]
+    
+sp500 = sp500.dropna()
+predictions = backtest(sp500, model, new_predictors)
 print(precision_score(predictions["Target"], predictions["Predictions"]))
